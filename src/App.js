3a45af1b924fae7414e06db4fd6bdd6da74e2429
dutra-86 <gabrielsprecherdutra@gmail.com>
2022-11-17 23:26:15 -0300

    Imagens consertadas

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import gag from './assets/img/9gag.svg'
-import adorable from './assets/img/9gag.svg'
-import bad from './assets/img/9gag.svg'
-import barked from './assets/img/9gag.svg'
-import cat from './assets/img/9gag.svg'
-import chi from './assets/img/9gag.svg'
-import dog from './assets/img/9gag.svg'
-import filomoderna from './assets/img/9gag.svg'
-import gato from './assets/img/9gag.svg'
+// import gag from './assets/img/9gag.svg'
+// import adorable from './assets/img/9gag.svg'
+// import bad from './assets/img/9gag.svg'
+// import barked from './assets/img/9gag.svg'
+// import cat from './assets/img/9gag.svg'
+// import chi from './assets/img/9gag.svg'
+// import dog from './assets/img/9gag.svg'
+// import filomoderna from './assets/img/9gag.svg'
+// import gato from './assets/img/9gag.svg'
 
 import Navbar from "./Navbar.js"
 import Corpo from "./Corpo.js"
