1bc1cc463a4c4b353768ce10f80f6141f4d58c25
dutra-86 <gabrielsprecherdutra@gmail.com>
2022-11-20 18:58:42 -0300

    Implementada função de alterar foto de perfil

--- src/Usuario.js
+++ src/Usuario.js
@@ -9,10 +9,11 @@ const usuario = {
 export default function Usuario(){
 
 	const [nome, setNome] = React.useState(usuario.name);
+	const [profPic, setPfp] = React.useState(usuario.img);
 
 	return(
 		<div class="usuario" data-test="user">
-		<img src={usuario.img} data-test="profile-img"/>
+		<img src={profPic} data-test="profile-img" onClick={changePfp}/>
 		<div class="texto">
 			<strong>{usuario.user}</strong>
 			<span>
@@ -28,6 +29,8 @@ export default function Usuario(){
 		newName? setNome(newName) : changeName();
 	}
 		
+	function changePfp(){
+		const newPfp = prompt("Insira o link da sua foto de perfil");
+		if (newPfp){setPfp(newPfp)};
+	}
 }
-
-//onClick={()=>setNome(prompt("Insira um novo nome"))
